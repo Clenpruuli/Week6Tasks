264a2b6fdb4ac2179433052b404aaa022d445c69
Opilane TTHK <opilane@tthk.ee>
2020-10-07 16:27:24 +0300

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[10];
+            int sum = 0;
+
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 11);
+            }
+          
+            foreach (int num in array)
+            {
+                sum = sum + num;
+            }
+            Console.WriteLine("Sum of array elements is:" + sum);
+
+
+
+        }
+    }
+}
